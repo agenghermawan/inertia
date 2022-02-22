f41a5151a493af725fb54ba05069d40e1fc8b965
Hermawan <agenghermawan16@gmail.com>
2022-02-22 07:25:14 +0700

    inertiaa

---
+++ resources/js/Pages/test.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function test() {
+    return (
+        <div>
+            <a href="/"> Back to Route asdaasdsaadsasdasdas </a>
+            <a href="/test"> Back to Test </a>
+        </div>
+    );
+}
